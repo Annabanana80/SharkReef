e98c709e2c91db2388f20efbede668562fd63487
Anna Sanderson <anna.sanderson@csuglobal.edu>
2016-10-20 01:06:12 -0400

    first commit

---
+++ js/init.js
@@ -0,0 +1,29 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+function div_show() {
+document.getElementById('abc').style.display = "block";
+}
+// Validating Empty Field
+function check_empty() {
+if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+alert("Fill All Fields !");
+} else {
+document.getElementById('form').submit();
+alert("Form Submitted Successfully...");
+}
+}
+//Function To Display Popup
+function div_show() {
+document.getElementById('abc').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide(){
+document.getElementById('abc').style.display = "none";
+}
