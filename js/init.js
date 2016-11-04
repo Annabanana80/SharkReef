da980dcc303b7f8e55accc3c58e590544044f4a7
Anna Sanderson <anna.sanderson@csuglobal.edu>
2016-11-04 18:25:05 -0400

    updated photos, added menu, added functionality to contact us button, still a work in progress

--- js/init.js
+++ js/init.js
@@ -7,23 +7,9 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-function div_show() {
-document.getElementById('abc').style.display = "block";
-}
-// Validating Empty Field
-function check_empty() {
-if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
-alert("Fill All Fields !");
-} else {
-document.getElementById('form').submit();
-alert("Form Submitted Successfully...");
-}
-}
-//Function To Display Popup
-function div_show() {
-document.getElementById('abc').style.display = "block";
-}
-//Function to Hide Popup
-function div_hide(){
-document.getElementById('abc').style.display = "none";
-}
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+  
+          
